5720823c7b50028daffd943b116b02297c67ed9e
Poul Kalff <poulkalff@gmail.com>
2017-03-21 15:08:34 +0100

    First commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp defaults.bind /usr/local/bin/
+cp g13d /usr/local/bin/
+cp g13interface /usr/local/bin/
+cp g13logo.png /usr/local/bin/
+cp g13interface.desktop /usr/share/applications/
+
+echo
+echo '  All done, please start by running "g13interface" from "/usr/local/bin" or from the dash!'
+echo
